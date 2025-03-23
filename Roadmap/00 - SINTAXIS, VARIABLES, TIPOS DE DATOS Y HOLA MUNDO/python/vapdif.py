649525f64e8bbe4918ce6a44b552240ad5fd1718
DiegoFVasquezP <vapdif@gmail.com>
2025-03-23 14:04:58 -0400

    #00-Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/vapdif.py
@@ -0,0 +1,67 @@
+# DESARROLLO EJERCICIO:
+
+# 1. Crea un comentario en el código y coloca la URL del sitio web oficial del 
+#       lenguaje de programación que has seleccionado.
+#--------------------------------------------------------------------------------
+
+# Sitio oficial de Python es "https://www.python.org/"
+
+"""Otro enlace de interés es 'https://docs.python.org/es/3.13/tutorial/index.html' """
+
+# 2. Representa las diferentes sintaxis que existen de crear comentarios
+#       en el lenguaje (en una línea, varias...).
+#--------------------------------------------------------------------------------
+
+# Comentario simple se inician con (almohadilla, numeral o hashtag) "#" al principio de la linea 
+
+"""
+para hacer un comentario en 
+varias lineas se puede utilizar 
+comillas dobles 
+"""
+
+# o
+
+'''
+comillas 
+sencillas
+'''
+
+# 3. Crea una variable (y una constante si el lenguaje lo soporta).
+#--------------------------------------------------------------------------------
+
+# se crea usando el nombre deseado y se le asigna un valor
+
+num = 0 # variable de tipo entero, lo cual python infiere por el tipo de dato introducido
+num_float = 1.5 # numero flotante
+
+"""
+Para representar una variable constante en Python (ya que el lenguaje no tiene constantes nativas),
+La convención es crearla en letra mayuscula y si se compone de varias palabras,
+se separan por un guion bajo, underscore '_'
+"""
+
+PI = 3.1416
+MI_ID = 123452
+
+
+# 4. Crea variables representando todos los tipos de datos primitivos
+#       del lenguaje (cadenas de texto, enteros, booleanos...).
+#--------------------------------------------------------------------------------
+
+entero = 123    # tipo int, entero
+
+flotante = 3.5632   # tipo flotante, float
+
+booleanos = True    # tipo "bool" valor puede ser True o False. iniciado con mayuscula
+
+texto = "variable tipo string o cadena de texto"
+
+
+"""
+para imprimir un valor lo podemos hacer con la funcion print() 
+seguido de parentesis 
+y el texto entre comillas 
+"""
+print("¡Hola, python!")
+# se imprimira unicamente lo que este dentro de las comillas
